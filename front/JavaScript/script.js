3eef59ec778a6dcbed95cf4631d9464b0f1e67f8
Beatriz Almeida <yakoosoba@gmail.com>
2026-05-22 20:25:03 -0500

    JavaScript

---
+++ front/JavaScript/script.js
@@ -0,0 +1,86 @@
+console.log("Boa noite, hoje é sexta-feira.")
+
+// console
+
+console.error("Este é um erro!")
+console.warn("Este é um aviso!")
+
+// variáveis
+
+var x = 10
+
+// variáveis modernas
+
+let y = 15
+
+const z = 20
+
+console.log(x)
+console.log(y)
+console.log(z)
+
+// tipos de dados
+
+//texto
+
+const name = "Morales Zoka Saiki Star "
+
+console.log(name)
+
+console.log(typeof name)
+
+// numérico
+
+const shirtsQty = 4
+
+console.log("shirtsQty")
+
+console.log(typeof shirtsQty)
+
+// número decimal
+
+const decimal = 20.1
+
+console.log("decimal")
+
+console.log(typeof decimal)
+
+// boolean (true or false)
+
+const isApproved = false
+
+console.log("isApproved")
+
+console.log(typeof isApproved)
+
+// null
+
+let surname = null
+
+console.log(surname)
+
+console.log(typeof surname)
+
+// array
+
+let languages = ["JavaScript", "PHP", "Phyton"]
+
+console.log(languages)
+
+console.log(typeof languages)
+
+// object
+
+const user = {email: "yakoosoba@gmail.com", password: "Alanzok1", age: 20}
+
+console.log(user)
+
+console.log(typeof user)
+
+// método de String
+
+const fullName = "Beatriz Morales Zoka Saiki Star"
+
+console.log(fullName.length)
+
+console.log(typeof fullName)
